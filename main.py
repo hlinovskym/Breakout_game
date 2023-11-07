939672c08860ef063e9513298d25760554024cdb
Mila <hlinovskym@seznam.cz>
2023-11-07 20:11:15 +0100

    without game_is_on

--- main.py
+++ main.py
@@ -42,75 +42,75 @@ for maze in mazes:
             bricks.append(brick)
 
     # to play the game
-    while game_is_on:
-        scoreboard.update_score(lives, score)
-        print(f"lives left: {lives}")
-        print("starting while")
-        game_starting = True
+    # while game_is_on:
+    scoreboard.update_score(lives, score)
+    print(f"lives left: {lives}")
+    print("starting while")
+    game_starting = True
 
-        # to move paddle together with ball before bouncing the ball
-        while game_starting:
-            screen.update()
-            time.sleep(0.1)
-            x_cor = paddle.xcor()
-            ball.set_position(x_cor)
-            game_starting = ball.starting
+    # to move paddle together with ball before bouncing the ball
+    while game_starting:
+        screen.update()
+        time.sleep(0.1)
+        x_cor = paddle.xcor()
+        ball.set_position(x_cor)
+        game_starting = ball.starting
 
-        game_running = True
-        print("running while")
+    game_running = True
+    print("running while")
 
-        # to let the ball running
-        while game_running:
-            screen.update()
-            time.sleep(0.1)
-            ball.move()
+    # to let the ball running
+    while game_running:
+        screen.update()
+        time.sleep(0.1)
+        ball.move()
 
-            # collision with side walls
-            if ball.xcor() > 280 or ball.xcor() < -280:
-                ball.x_switch()
+        # collision with side walls
+        if ball.xcor() > 280 or ball.xcor() < -280:
+            ball.x_switch()
 
-            # collision with top wall
-            if ball.ycor() > 280:
+        # collision with top wall
+        if ball.ycor() > 280:
+            ball.y_switch()
+
+        # collision with paddle
+        if ball.distance(paddle) < 30 and ball.ycor() < -230:
+            ball.y_switch()
+
+        if 30 < ball.distance(paddle) < 60 and ball.ycor() < -230 and ball.xcor() > paddle.xcor():
+            ball.y_switch()
+            ball.plus_x()
+
+        if 30 < ball.distance(paddle) < 60 and ball.ycor() < -230 and ball.xcor() < paddle.xcor():
+            ball.y_switch()
+            ball.minus_x()
+
+        # falling to the bottom
+        if ball.ycor() < -300:
+            game_running = False
+            ball.reset_position()
+            lives -= 1
+
+        # collision with brick
+        for brick in bricks:
+            if abs(ball.ycor() - brick.ycor()) < 20 and abs(ball.xcor() - brick.xcor()) < 60:
+                bricks.remove(brick)
+                brick.goto(1080, 0)
                 ball.y_switch()
+                print(f"bricks left: {len(bricks)}")
 
-            # collision with paddle
-            if ball.distance(paddle) < 30 and ball.ycor() < -230:
-                ball.y_switch()
+        # out of bricks
+        if len(bricks) == 0:
+            score += 1
+            ball.reset_position()
+            scoreboard.you_won(score)
+            game_running = False
+            # game_is_on = False
 
-            if 30 < ball.distance(paddle) < 60 and ball.ycor() < -230 and ball.xcor() > paddle.xcor():
-                ball.y_switch()
-                ball.plus_x()
-
-            if 30 < ball.distance(paddle) < 60 and ball.ycor() < -230 and ball.xcor() < paddle.xcor():
-                ball.y_switch()
-                ball.minus_x()
-
-            # falling to the bottom
-            if ball.ycor() < -300:
-                game_running = False
-                ball.reset_position()
-                lives -= 1
-
-            # collision with brick
-            for brick in bricks:
-                if abs(ball.ycor() - brick.ycor()) < 20 and abs(ball.xcor() - brick.xcor()) < 60:
-                    bricks.remove(brick)
-                    brick.goto(1080, 0)
-                    ball.y_switch()
-                    print(f"bricks left: {len(bricks)}")
-
-            # out of bricks
-            if len(bricks) == 0:
-                score += 1
-                ball.reset_position()
-                scoreboard.you_won(score)
-                game_running = False
-                game_is_on = False
-
-            # out of lives
-            if lives == 0:
-                scoreboard.game_over(score)
-                game_running = False
-                game_is_on = False
+        # out of lives
+        if lives == 0:
+            scoreboard.game_over(score)
+            game_running = False
+            # game_is_on = False
 
 screen.exitonclick()
